bc411ef65472491a1ce86cfdf468e300c7a636d6
Trần Thanh Nhân <66198815+nhanv-dev@users.noreply.github.com>
2023-03-15 19:11:24 +0700

    tabs in creating product, changing password

--- src/pages/shop/product-creating/Editor.jsx
+++ src/pages/shop/product-creating/Editor.jsx
@@ -1,16 +1,25 @@
 import {CKEditor} from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { makeStyles } from '@material-ui/core/styles';
 
 function Editor({product,value}) {
-
+    const useStyles = makeStyles((theme) => ({
+        richTextEditor: {
+            "& .ck-editor__main > .ck-editor__editable": {
+                minHeight: "350px"
+            }
+        }
+    }));
+    const classes = useStyles();
     return (
-        <div className="editor">
+        <div className={classes.richTextEditor}>
             <CKEditor
                 editor={ClassicEditor}
                 data={product[value]}
                 onChange={(event, editor) => {
                     product[value] = editor.getData();
                 }}
+                height="300"
             />
         </div>
     );
