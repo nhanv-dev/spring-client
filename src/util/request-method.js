02038dd9f2680b9a173904160fe6d82558c946b5
Trần Thanh Nhân <66198815+nhanv-dev@users.noreply.github.com>
2023-09-17 00:17:35 +0700

    update

--- src/util/request-method.js
+++ src/util/request-method.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 import { getItem } from "./localStorage";
 
-// export const baseURL = "http://springboot-app.back-net:8080/api/";
-// export const baseURL = "https://glorious-route-production.up.railway.app/api/";
 export const baseURL = `${process.env.REACT_APP_SPRING_URL}` || "http://localhost:8080/api/";
 
 export const publicRequest = () => {
@@ -14,7 +12,7 @@ export const publicRequest = () => {
 
 export const protectedRequest = () => {
     const token = getItem("user")?.token
-    console.log(process.env.REACT_APP_SPRING_URL, baseURL);
+    console.log(baseURL);
     return axios.create({
         baseURL: baseURL,
         headers: { Authorization: `Bearer ${token}` },
